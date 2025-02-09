ba6c2ccef87fa8bb201c467c97d6e3df1d524ebe
Waris-Al <92109525+Waris-Al@users.noreply.github.com>
2025-02-09 14:47:14 +0000

    Login/Register Skeletons + Formatting clean up

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/Login')
+def Login():
+    return render_template('login.html')
+
+@app.route('/Register')
+def Register():
+    return render_template('register.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
 
