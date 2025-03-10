b49372e3909f518e6a33f5ca714c0276b3dd1a12
Waris-Al <92109525+Waris-Al@users.noreply.github.com>
2025-03-10 13:37:24 +0000

    search for albums to review works

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, url_for, request, redirect, session, jsonify
 from database import registerUser, init_db, logUserIn, addReview, usersreviews
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 import os
 app = Flask(__name__)
 
 #Stuff to load database
 load_dotenv(dotenv_path='env/.env')
 app.secret_key = os.getenv("app.secret_key")
+
+spotifyClientID = os.getenv("spotifyClientID")
+spotifyClientSecret = os.getenv("spotifyClientSecret")
+
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv("dbMasterUsername")}:{os.getenv("dbMasterPassword")}@localhost/dbname'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 init_db(app)
@@ -14,6 +20,52 @@ init_db(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+REDIRECT_URI = "http://127.0.0.1:5000/callback" 
+
+
+SCOPE = "user-top-read user-read-playback-state user-read-currently-playing"
+
+
+
+sp_oauth = SpotifyOAuth(client_id=spotifyClientID,
+                        client_secret=spotifyClientSecret,
+                        redirect_uri=REDIRECT_URI,
+                        scope=SCOPE)
+
+
+@app.route("/spotify_login")
+def spotify_login():
+    """Redirect user to Spotify OAuth login"""
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
+@app.route("/callback")
+def callback():
+    """Handle Spotify OAuth callback and retrieve access token"""
+    code = request.args.get("code")
+
+    if not code:
+        return "Error: No authorization code provided by Spotify."
+
+    try:
+
+        token_info = sp_oauth.get_access_token(code, as_dict=True)
+
+        print("Token Info:", token_info)  
+
+        if not token_info or "access_token" not in token_info:
+            return "Error: Failed to retrieve access token from Spotify. Try logging in again."
+
+        session["token_info"] = token_info
+        return redirect(url_for("top_artist"))
+
+    except Exception as e:
+        print("Spotify Token Error:", str(e))
+        return f"Error: {str(e)}"
+
+
 
 @app.route('/Homepage')
 def Homepage():
@@ -80,6 +132,13 @@ def save_review():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
+@app.route('/newReview')
+def newReview():
+    return render_template("newReview.html", spotifyClientID=spotifyClientID, spotifyClientSecret=spotifyClientSecret)
+
+@app.route('/test')
+def test():
+    return render_template("test.html", spotifyClientID=spotifyClientID, spotifyClientSecret=spotifyClientSecret)
 
 if __name__ == "__main__":
     app.run(debug=True)
