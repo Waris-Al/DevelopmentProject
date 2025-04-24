95e2ec16aef63fbbe75fe99d57fa67037d3f1a35
Waris-Al <92109525+Waris-Al@users.noreply.github.com>
2025-04-24 16:41:43 +0100

    model uses appropriate tokenizer

--- routes/model.py
+++ routes/model.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import LabelEncoder
 from flask import Blueprint, request
 from dotenv import load_dotenv
 import os
+import pickle
 
 
 load_dotenv(dotenv_path='env/.env') #Access env variables
@@ -14,16 +15,13 @@ AIStarPrediction = Blueprint('AIStarPrediction', __name__)
 @AIStarPrediction.route('/modelTesting', methods=['POST'])
 def useModel():
     text_input = request.get_json().get('reviewContent')
-# Load model
-    print(os.listdir())
+    #Load model and tokenizer
     model = load_model('AIModels/predictStarRatingModel.h5')
+    with open('AIModels/trainingTokenizer.pkl', 'rb') as f:
+        tokenizer = pickle.load(f)
 
-    #this is only tokenizing the inputted text: load the tokenizer from training instead
     texts = [text_input]  
-    max_words = 10000
     max_len = 200
-    tokenizer = Tokenizer(num_words=max_words, oov_token="<OOV>")
-    tokenizer.fit_on_texts(texts)
 
     label_encoder = LabelEncoder()
     label_encoder.fit(["1", "2", "3", "4", "5"])  #our possible values
